f443b3d3d3626762a81522be5834d563ca9314e0
BHATDV <BHATDV@AZ0245D>
2016-11-03 16:47:56 -0500

    Need info

---
+++ SampleAzureWebApplication/SampleAzureWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleAzureWebApplication.Startup))]
+namespace SampleAzureWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
