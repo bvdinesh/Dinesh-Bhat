1217fbebfca2712dd90381f241e8d261c57f5e63
BHATDV <BHATDV@AZ0245D>
2016-11-13 17:39:33 -0600

    commit

--- SampleAzureWebApplication/SampleAzureWebApplication/Controllers/HomeController.cs
+++ SampleAzureWebApplication/SampleAzureWebApplication/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using SampleAzureWebApplication.Models;
 namespace SampleAzureWebApplication.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("People");
+            TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Doe"));
+            // Print the fields for each customer.
+            List<CustomerEntity> lstEnt = new List<CustomerEntity>();
+
+            foreach (CustomerEntity entity in table.ExecuteQuery(query))
+            {
+                CustomerEntity ent = new CustomerEntity(entity.PartitionKey, entity.RowKey);
+                ent.Email = entity.Email;
+                ent.PhoneNumber = entity.PhoneNumber;
+                lstEnt.Add(ent);
+            }
+            return View(lstEnt);
         }
 
         public ActionResult About()
